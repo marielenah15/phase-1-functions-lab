bbe41c78d0dc69aec09ea76d4fc5fe0a60d03653
Marielena Hernandez <marielenah15@gmail.com>
2021-12-14 12:19:57 -0600

    commit tree b784e0fb664b81e97f9b101f170b1abd85d41621

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(42 - street);
+}
+
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(street){
+    return distanceFromHqInBlocks(street) * 264; 
+}
